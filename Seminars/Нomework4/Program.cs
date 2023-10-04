539f839462bde59351195a172a35c73d9c6cdbb7
Oksana-deva <144332419+Oksana-deva@users.noreply.github.com>
2023-10-04 15:19:24 +0300

    добавили домашнее задание4

---
+++ Seminars/Нomework4/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 25: Напишите цикл, который принимает 
+// на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// // 2, 4 -> 16
+
+
+
+// Console.Write("Введите число A: ");
+//       int A = Convert.ToInt32(Console.ReadLine());
+//       Console.Write("Введите число B: ");
+//       int B = Convert.ToInt32(Console.ReadLine());
+
+//       int res = 1;
+
+//       for (int i = 0; i < B; i++)
+//       {
+//         res *= A;
+//       }
+
+//     Console.WriteLine("A^B = " + res);
+
+
+// // Задача 27: Напишите программу, 
+// // которая принимает на вход число
+// //  и выдаёт сумму цифр в числе.
+// // 452 -> 11
+// // 82 -> 10
+// // 9012 -> 12
+
+
+// Console.Write("Введите число: ");
+//     string num = Console.ReadLine();
+
+//     int res = 0;
+
+//     for (int i = 0; i < num.Length; i++)
+//     {
+//         res += (num[i] - '0'); // (num[i] - '0')  (char => int)
+//     }
+
+// //     Console.WriteLine(res);
+
+
+// // // Задача 29: Напишите программу,
+// //  которая задаёт массив из 8 элементов 
+// //  и выводит их на экран.
+// // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// // 6, 1, 33 -> [6, 1, 33]
+
+
+// int[] array = { 1, 5, 9, 0, 4, 45, -35, 8 };
+
+//     Console.Write("[");
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         Console.Write(array[i] + ", ");
+//     }
+//     Console.Write(array[array.Length - 1]);
+//     Console.Write("]");
+
